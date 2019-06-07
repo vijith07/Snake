d1f75b1905cbcce8b70fdd0c14c3edc1293fbe83
Shriram <18211a04l2@bvrit.ac.in>
2019-06-07 16:40:40 +0530

    added gameover flag to snake class

--- snake.py
+++ snake.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 class Food:
-    
     def __init__(self,x,y):
         self.posx=random.randint(0,25)
         self.posy=random.randint(0,25)
@@ -18,16 +17,29 @@ class Snake:
          self.posy=y
          self.velx=0
          self.vely=0
+         self.gameover=False
     def update(self):   
          self.posx+=self.velx
          self.posy+=self.vely
          if((food.posx==self.posx) and (food.posy == self.posy)):
              food.reset()
+         if (self.posx>24):
+             self.gameover=True
+         if (self.posx<0):
+             self.gameover=True
+         if (self.posy>24):
+             self.gameover=True
+         if (self.posy<0):
+             self.gameover=True
     def draw(self,renderer):
          pygame.draw.rect(renderer,BLACK,(20*self.posx,20*self.posy,20,20))
 
-     
-         
+    def reset(self):
+        self.posx=0
+        self.posy=0
+        self.velx=0
+        self.vely=0
+        self.gameover=False
 
 pygame.init()
 timeelapsed=0
@@ -67,8 +79,12 @@ while (isRunning):
     if timeelapsed>100:
         snake.update()
         timeelapsed=0
-    snake.draw(renderer)   
-    food.draw(renderer)
+    if(not snake.gameover):
+        snake.draw(renderer)   
+        food.draw(renderer)
+    else:
+        snake.reset()
+        food.reset()
     pygame.display.update()
 pygame.quit()
 
